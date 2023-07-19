458268691b11e5eb072c4059f72930ba90767032
minsuki2 <minsuki2@student.42seoul.kr>
2023-07-19 21:39:54 +0900

    fix mariadb

--- inception/srcs/requirements/mariadb/tools/script.sh
+++ inception/srcs/requirements/mariadb/tools/script.sh
@@ -1,14 +1,32 @@
-#!/bin/bash
+#!/bin/sh
 
-chown -R mysql:mysql /var/lib/mysql
+chown -R 777 /var/lib/mysql
 service mariadb start
 
-echo "CREATE DATABASE IF NOT EXISTS $db1_name ;" > /root/init.sql
-echo "CREATE USER IF NOT EXISTS '"$db1_user"'@'%' IDENTIFIED BY '"$db1_pwd"' ;" >> /root/init.sql
-echo "GRANT ALL PRIVILEGES ON $db1_name.* TO '"$db1_user"'@'%' ;" >> /root/init.sql
-echo "FLUSH PRIVILEGES;" >> /root/init.sql
-mysql < /root/init.sql
-rm /root/init.sql
+echo "CREATE DATABASE IF NOT EXISTS $DB_NAME;" > /tmp/init.sql
+echo "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PWD';" >> /tmp/init.sql
+echo "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%';" >> /tmp/init.sql
+echo "FLUSH PRIVILEGES;" >> /tmp/init.sql
+mysql < /tmp/init.sql
+rm /tmp/init.sql
 
-service mariadb stop
+service mariadb stop #root로 실행시키기 
 mariadbd --user=root
+
+
+# chmod -R 777 /var/lib/mysql/
+#
+# service mysql start
+#
+# if [ ! -d "/var/lib/mysql/$DB_NAME" ];then
+# 	mysql -e "CREATE DATABASE IF NOT EXISTS $DB_NAME";
+# 	mysql -e "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PWD'";
+# 	mysql -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%'";
+# 	mysql -e "FLUSH PRIVILEGES;"
+# 	mysql -e "ALTER USER '$MYSQL_ROOT'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';"
+# 	mysql -e "FLUSH PRIVILEGES;"
+# 	mysql $DB_NAME -u$MYSQL_ROOT -p$MYSQL_ROOT_PASSWORD < /tmp/wpdb_dump.sql
+# fi
+#
+#
+#
--- inception/srcs/requirements/wordpress/tools/script.sh
+++ inception/srcs/requirements/wordpress/tools/script.sh
@@ -1,12 +1,10 @@
-#!/bin/bash
-mkdir -p /var/www/html
-
-cd /var/www/html
-pwd
-
-# 기존에 있으면 삭제
-rm -rf *
-
+#!/bin/sh
+# mkdir -p /var/www/html
+#
+# cd /var/www/html
+#
+# # 기존에 있으면 삭제
+# rm -rf *
 # WP-CLI를 다운로드합니다.
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 
@@ -14,16 +12,26 @@ curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.pha
 chmod +x wp-cli.phar
 
 # WP-CLI를 전역적으로 사용할 수 있도록 이동합니다.
-mv wp-cli.phar /usr/local/bin/wp
+mv wp-cli.phar /usr/local/bin/
+ln -sf /usr/local/bin/wp-cli.phar /usr/local/bin/wp-cli
+
+ls
 
 # WordPress Core를 다운로드합니다.
-wp core download --allow-root
+mkdir -p /var/www/wordpress
 
-# wp-config.php 파일을 생성합니다.
-wp config create --dbname=$db1_name --dbuser=$db1_user --dbpass=$db1_pwd --dbhost=my_mariadb:3306 --allow-root
+if [ -z "$(ls -A /var/www/wordpress)" ]; then
+    echo "< Here is Empty"
+    cd /var/www/wordpress
+    wp-cli --allow-root core download 
+    wp-cli config create --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PWD --dbhost=$(DB_HOST) --allow-root
+    wp-cli core install --url=localhost --title=$WP_TITLE --admin_user=$WP_ADMIN_USR --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
+else
+    echo "< Here is already installed"
+fi
+# wp-cli-config.php 파일을 생성합니다.
 # WordPress를 설치합니다.
-wp core install --url=localhost --title=$WP_TITLE --admin_user=$WP_ADMIN_USR --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
 
-service php7.4-fpm start
+php-fpm7.3 -F
 
-while true; do sleep 30; done;
+#while true; do sleep 30; done;
